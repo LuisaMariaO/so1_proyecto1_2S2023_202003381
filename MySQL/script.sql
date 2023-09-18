b43055231f0be4a9aa6edc796dea3911d262fbb9
Luisa María Ortíz Romero <78897324+LuisaMariaO@users.noreply.github.com>
2023-09-18 08:55:44 -0600

    [Update] script.sql

--- MySQL/script.sql
+++ MySQL/script.sql
@@ -1,12 +1,22 @@
 CREATE DATABASE proyecto1;
 USE proyecto1;
 
-CREATE TABLE ram (
-    id TIMESTAMP PRIMARY KEY,
-    porcentaje INT
+CREATE TABLE lectura (
+    fecha_hora TIMESTAMP NOT NULL,
+    ram        INTEGER NOT NULL,
+    cpu        INTEGER NOT NULL,
+    vm_ip      VARCHAR2(30) NOT NULL
 );
 
-CREATE TABLE cpu (
-    id TIMESTAMP PRIMARY KEY,
-    porcentaje INT
+ALTER TABLE lectura ADD CONSTRAINT lectura_pk PRIMARY KEY ( fecha_hora,
+                                                            vm_ip );
+
+CREATE TABLE vm (
+    ip VARCHAR2(30) NOT NULL
 );
+
+ALTER TABLE vm ADD CONSTRAINT vm_pk PRIMARY KEY ( ip );
+
+ALTER TABLE lectura
+    ADD CONSTRAINT lectura_vm_fk FOREIGN KEY ( vm_ip )
+        REFERENCES vm ( ip );
