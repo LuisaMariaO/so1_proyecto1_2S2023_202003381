62a6ab8f01bb492e3115b1be907c26529c3d0b9a
LuisaMariaO <luisamaria.ortiz17@gmail.com>
2023-09-17 19:50:07 -0600

    [ADD]MySql docker compose y script

---
+++ MySQL/script.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE proyecto1;
+USE proyecto1;
+
+CREATE TABLE ram (
+    id TIMESTAMP PRIMARY KEY,
+    porcentaje INT
+);
+
+CREATE TABLE cpu (
+    id TIMESTAMP PRIMARY KEY,
+    porcentaje INT
+);
